97f20134cf6320f9f2594fff01628eb30df2a608
Christina <chrasse@hotmail.com>
2022-02-21 20:48:56 +0100

    first steps on the FE part

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+      serif: ["Playfair Display", "serif"]
+    },
+    extend: {
+      colors: {
+        primary: "#11172d",
+        secondary: "#878a98",
+        neutral: "#f5f5f5"
+      }
+    }
   },
-  plugins: []
+  plugins: [require("@tailwindcss/forms")]
 }
