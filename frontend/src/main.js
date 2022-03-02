adda63cabab698d6594fa944e7bc1f524ef19498
Christina <chrasse@hotmail.com>
2022-03-02 19:49:33 +0100

    apollo and vue-apollo installed to make graphql queries

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,20 @@
-import { createApp } from "vue"
+import { createApp, provide, h } from "vue"
 import App from "./App.vue"
 import "./index.css"
 
-createApp(App).mount("#app")
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client"
+import { DefaultApolloClient } from "@vue/apollo-composable"
+
+const defaultClient = new ApolloClient({
+  uri: "http://localhost:1337/graphql",
+  cache: new InMemoryCache()
+})
+
+createApp({
+  setup() {
+    provide(DefaultApolloClient, defaultClient)
+  },
+  render() {
+    return h(App)
+  }
+}).mount("#app")
