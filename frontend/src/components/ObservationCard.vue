2dd04b5483c42e64488ddd887b62c24e31b4d63c
Christina <chrasse@hotmail.com>
2022-03-23 22:21:36 +0100

    started the observation view

---
+++ frontend/src/components/ObservationCard.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  props: {
+    id: String,
+    time: String,
+    temperature: Number,
+    weather: String,
+    wind_direction: String,
+    wind_speed: Number,
+    cloud_cover: String,
+    cloud_types: String,
+    personal: String
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>Observation Card</h1>
+    {{ weather }}
+    {{ personal }}
+  </div>
+</template>
--- frontend/src/views/MyObservations.vue
+++ frontend/src/views/MyObservations.vue
@@ -1,5 +1,58 @@
-<script></script>
+<script>
+import { ref, watch } from "vue"
+import CloudButton from "../components/ui/CloudButton.vue"
+import { useQuery, useResult } from "@vue/apollo-composable"
+import getUserObservations from "../graphql/queries/getUserObservations.query.graphql"
+import { useUserData } from "../../providers/userProvider"
+import ObservationCard from "../components/ObservationCard.vue"
+
+export default {
+  components: { ObservationCard },
+  setup() {
+    const { getId } = useUserData()
+
+    const { result: userObsResult, error } = useQuery(
+      getUserObservations,
+      () => ({
+        userId: getId.value
+      })
+    )
+    const userObsList = useResult(
+      userObsResult,
+      [],
+      data => data.observations.data
+    )
+
+    watch(
+      () => userObsList.value,
+      () => {
+        console.log("Obsar, ", userObsList.value)
+      }
+    )
+
+    return { userObsList }
+  }
+}
+</script>
 
 <template>
-  <h1 class="my-10 text-violet-600">My obz</h1>
+  <h1 class="my-4 text-3xl text-center lg:my-6">My latest observations</h1>
+  <div>{{ userObsList }}</div>
+  <template v-if="userObsList.length > 0">
+    <span>This will show</span>
+
+    <observation-card
+      v-for="obs in userObsList"
+      :key="obs.id"
+      :id="obs.id"
+      :time="obs.attributes.observation_time"
+      :temperature="obs.attributes.temperature"
+      :weather="obs.attributes.weather"
+      :wind_direction="obs.attributes.wind_direction"
+      :wind_speed="obs.attributes.wind_speed"
+      :cloud_cover="obs.attributes.cloud_cover"
+      :cloud_types="obs.attributes.cloud_types"
+      :personal="obs.attributes.personal"
+    />
+  </template>
 </template>
